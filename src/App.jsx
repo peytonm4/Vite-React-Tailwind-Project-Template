2a3b149686c43d66b426368fb35d443e00da4cf1
Peyton McCutcheon <peytonmccutcheon4@gmail.com>
2024-04-25 18:37:06 -0700

    Global git credentials test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <h1 className="text-4xl text-red-500 font-bold underline">
-      Hello world!!!
+      Hello world!!!!!!!!!!!!!!!!!!!!!!!!
     </h1>
   )
 }
